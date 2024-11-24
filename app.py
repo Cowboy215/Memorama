9a3d3b6b10455c8cbb794bf8050e63d5a6e21612
davidhdx <david.delarosa@cimat.mx>
2024-11-24 09:21:14 -0600

    Arreglado los bugs de selección reiterada de cartas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from src.controllers.usuario_controller import insertar_usuario, obtener_usuarios, borrar_usuario, actualizar_usuario
+# from src.controllers.usuario_controller import insertar_usuario, obtener_usuarios, borrar_usuario, actualizar_usuario
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
 app = Flask(__name__)
@@ -7,20 +7,23 @@ app.secret_key = '123459384'  # Necesario para manejar sesiones
 
 # Definir las cartas para cada dificultad
 difficulties = {
-    'easy': ['imagen1.png', 'imagen2.png', 'imagen3.png'],
-    'medium': ['imagen1.png', 'imagen2.png', 'imagen3.png', 'imagen4.png', 'imagen5.png', 'imagen6.png'],
-    'hard': ['imagen1.png', 'imagen2.png', 'imagen3.png', 'imagen4.png', 'imagen5.png', 'imagen6.png',
-             'imagen7.png', 'imagen8.png', 'imagen9.png', 'imagen10.png']
+    'easy':   [f'imagen{i}.png' for i in range(1,4)],
+    'medium': [f'imagen{i}.png' for i in range(1,7)],
+    'hard':   [f'imagen{i}.png' for i in range(1,11)],
 }
+
+initial_scores = {'easy': 30, 'medium': 60, 'hard': 100}
 
 messages = {
     'win': "¡Felicidades! Has encontrado todos los pares.",
     'loss': "Lo siento, has perdido. Se acabaron tus intentos."
 }
 
+default_values = {"result": "loss", "player": None, "final_score": 0, "difficulty": None}
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template('difficulty.html')
 
 
@@ -34,37 +37,39 @@ def play():
         random.shuffle(cards)  # Mezclar las cartas
 
         session['cards'] = cards
-
-        if difficulty == 'easy':
-            session['score'] = 30
-        elif difficulty == 'medium':
-            session['score'] = 60
-        elif difficulty == 'hard':
-            session['score'] = 100
+        session['score'] = initial_scores[difficulty]
 
         return redirect(url_for('play'))
-    return render_template('index.html', cards=session['cards'])
-
-#
+    
+    if request.method == 'GET':
+        return render_template('index.html', cards=session['cards'])
 
 
 @app.route('/end', methods=['GET', 'POST'])
 def end():
     if request.method == 'POST':
         data = request.get_json()
-        result = data.get('status')
-        session['result'] = result
 
-        # Guardar el resultado en la base de datos
-        nombre_usuario = session.get('nombre', 'Usuario Anónimo')
-        puntuacion = session.get('score', 0)
-        dificultad = session.get('difficulty', 'easy') #falta corregir estos gets ;D
-        insertar_usuario(nombre_usuario, puntuacion, 0, dificultad)
+        for type in default_values.keys():
+            information = data.get(type, default_values[type])
+            print(information)
+            if information:
+                session[type] = session.get(type, information)
 
-        return redirect(url_for('end'))
+        if session.get("player", None):
+            return redirect(url_for('end'))
+        else:
+            
+            nombre_usuario = session.get('player')
+            puntuacion = session.get('score', 0)
+            dificultad = session.get('difficulty', 'easy') #falta corregir estos gets ;D
+            print(nombre_usuario, puntuacion, dificultad)
+            # insertar_usuario(nombre_usuario, puntuacion, 0, dificultad)
+            return redirect(url_for('end'))
 
     if request.method == 'GET':
         result = session.get('result')
+
         if not result:
             return redirect(url_for('index'))
 
@@ -72,34 +77,34 @@ def end():
 
 
 
-@app.route('/usuarios', methods=['GET']) #Metodos crud para crear la tabla :D
-def listar_usuarios():
-    usuarios = obtener_usuarios()
-    usuarios_json = [{"nombre": u.nombre, "puntuacion": u.puntuacion,
-                      "tiempo": u.tiempo, "dificultad": u.dificultad} for u in usuarios]
-    return jsonify(usuarios_json), 200
+# @app.route('/usuarios', methods=['GET']) #Metodos crud para crear la tabla :D
+# def listar_usuarios():
+#     usuarios = obtener_usuarios()
+#     usuarios_json = [{"nombre": u.nombre, "puntuacion": u.puntuacion,
+#                       "tiempo": u.tiempo, "dificultad": u.dificultad} for u in usuarios]
+#     return jsonify(usuarios_json), 200
 
 
-@app.route('/usuarios', methods=['POST'])
-def crear_usuario():
-    data = request.get_json()
-    insertar_usuario(data['nombre'], data.get('puntuacion', 0), data.get(
-        'tiempo', 0), data.get('dificultad', 'easy'))
-    return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
+# @app.route('/usuarios', methods=['POST'])
+# def crear_usuario():
+#     data = request.get_json()
+#     insertar_usuario(data['nombre'], data.get('puntuacion', 0), data.get(
+#         'tiempo', 0), data.get('dificultad', 'easy'))
+#     return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
 
 
-@app.route('/usuarios/<int:id_usuario>', methods=['DELETE'])
-def eliminar_usuario(id_usuario):
-    borrar_usuario(id_usuario)
-    return jsonify({"mensaje": "Usuario eliminado exitosamente"}), 200
+# @app.route('/usuarios/<int:id_usuario>', methods=['DELETE'])
+# def eliminar_usuario(id_usuario):
+#     borrar_usuario(id_usuario)
+#     return jsonify({"mensaje": "Usuario eliminado exitosamente"}), 200
 
 
-@app.route('/usuarios/<int:id_usuario>', methods=['PUT'])
-def modificar_usuario(id_usuario):
-    data = request.get_json()
-    actualizar_usuario(id_usuario, data['nombre'], data.get(
-        'puntuacion', 0), data.get('tiempo', 0), data.get('dificultad', 'easy'))
-    return jsonify({"mensaje": "Usuario actualizado exitosamente"}), 200
+# @app.route('/usuarios/<int:id_usuario>', methods=['PUT'])
+# def modificar_usuario(id_usuario):
+#     data = request.get_json()
+#     actualizar_usuario(id_usuario, data['nombre'], data.get(
+#         'puntuacion', 0), data.get('tiempo', 0), data.get('dificultad', 'easy'))
+#     return jsonify({"mensaje": "Usuario actualizado exitosamente"}), 200
 
 
 if __name__ == '__main__':
